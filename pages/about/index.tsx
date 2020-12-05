ae5cd65650e13b9fc66da4c93459d6b93c51f6e1
Craig Patik <craig@patik.com>
2020-12-05 15:11:18 +0100

    prettify

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 
 function Author() {
     return (
@@ -10,7 +10,7 @@ function Author() {
                 then quickly started coding as well.
             </p>
         </>
-    );
+    )
 }
 
-export default Author;
+export default Author
--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,13 +1,13 @@
-import React from 'react';
+import React from 'react'
 
 function PostTemplate(props) {
-    return <div>Here we'll load "{props.slug}"</div>;
+    return <div>Here we'll load "{props.slug}"</div>
 }
 
 PostTemplate.getInitialProps = async (context) => {
-    const { slug } = context.query;
+    const { slug } = context.query
 
-    return { slug };
-};
+    return { slug }
+}
 
-export default PostTemplate;
+export default PostTemplate
