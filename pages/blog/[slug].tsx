9f166b5b30e96af31e2af8b8d6ead98d3931e7d9
Craig Patik <craig@patik.com>
2020-12-05 15:23:13 +0100

    finished blog setup; formats MD files

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,11 +1,30 @@
 import React from 'react'
+import ReactMarkdown from 'react-markdown'
+import matter from 'gray-matter'
 
-function PostTemplate(props) {
-    return <div>Here we'll load "{props.slug}"</div>
+function PostTemplate({ content, data }) {
+    // This holds the data between `---` from the .md file
+    const frontmatter = data
+
+    return (
+        <>
+            <h1>{frontmatter.title}</h1>
+            <ReactMarkdown source={content} />
+        </>
+    )
 }
 
 PostTemplate.getInitialProps = async (context) => {
     const { slug } = context.query
+
+    // Import our .md file using the `slug` from the URL
+    const content = await import(`./content/${slug}.md`)
+
+    // Parse .md data through `matter`
+    const data = matter(content.default)
+
+    // Pass data to our component props
+    return { ...data }
 
     return { slug }
 }
