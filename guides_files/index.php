a92bca791cb6f120a6e1ccd2142a6da0c7ce3aa1
Craig Patik <craig@patik.com>
2023-01-03 20:36:50 +0100

    delete lots of stuff I won't be migrating

--- guides_files/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-header( 'Location: http://patik.com/index.php?page=guides' ) ;
-?>
--- pc/greasemonkey-user-scripts/guardian_fantasy_football_autoclick.user.js
+++
@@ -1,9 +0,0 @@
-// ==UserScript==
-// @name           Guardian Fantasy Football go directly to my team
-// @namespace      http://http://www.guardian.co.uk/fantasyfootball
-// @description    Goes directly to your team instead of making you click on "Click here to play now"
-// @include        http://www.guardian.co.uk/fantasyfootball
-// ==/UserScript==
-
-
-window.location.href="http://users.guardian.co.uk/ticketing/status/ffc?returnurl=http://fantasyfootball.guardian.co.uk/CleverTV/en/guardian/football/19/guardian.do";
