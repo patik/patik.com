7a78128ce98a2da9851202fc2d58f6edd5a40335
Craig Patik <craig@patik.com>
2023-01-29 16:53:10 +0100

    moved everything else out of old folder

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,33 @@
 import type { AppProps } from 'next/app'
+import NextHead from 'next/head'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+import config from '../blog/config.json'
 import '../styles/site/globals.css'
 import '../styles/site/scss/style.scss'
 
+const { description, basePath } = config
+
 export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
+    const router = useRouter()
+
+    useEffect(() => {
+        if (router.asPath.startsWith(basePath)) {
+            console.log('loading blog styles')
+
+            import('../blog/styles/index.css')
+            import('../blog/styles/post-body.css')
+        } else {
+            console.log('keeping online site styles')
+        }
+    }, [])
+
+    return (
+        <>
+            <NextHead>
+                <title>{description}</title>
+            </NextHead>
+            <Component {...pageProps} />
+        </>
+    )
 }
