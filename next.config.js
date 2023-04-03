0f63479e1bff1697140f563f59953261a0e46326
Craig Patik <craig@patik.com>
2023-04-03 19:30:15 +0200

    changes

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
-import path from 'path'
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
