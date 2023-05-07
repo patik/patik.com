e07fea5c3652453d7c67b01842d744362d5c0d0a
Craig Patik <craig@patik.com>
2023-05-07 21:45:43 +0200

    moved createGetStaticProps to its own file and made it generic

--- pages/travel/uzbekistan/photos/p/[photoId].tsx
+++ pages/travel/uzbekistan/photos/p/[photoId].tsx
@@ -1,8 +1,6 @@
 import { createGetStaticPaths } from '@src/photos/createGetStaticPaths'
+import { createGetStaticProps } from '@src/photos/createGetStaticProps'
 import { PhotoPage } from '@src/photos/PhotoPage'
-import getResults from '@src/photos/utils/cachedImages'
-import cloudinary from '@src/photos/utils/cloudinary'
-import getBase64ImageUrl from '@src/photos/utils/generateBlurPlaceholder'
 import type { ImageProps } from '@src/photos/utils/types'
 import type { GetStaticProps } from 'next'
 import { FC } from 'react'
@@ -17,33 +15,9 @@ const Page: FC<Props> = ({ currentPhoto }: Props) => {
 export default Page
 
 export const getStaticProps: GetStaticProps = async (context) => {
-    const results = await getResults(folderName)
-    const reducedResults: ImageProps[] = []
-    let i = 0
-
-    for (const result of results.resources) {
-        reducedResults.push({
-            id: i,
-            height: String(result.height),
-            width: String(result.width),
-            public_id: result.public_id,
-            format: result.format,
-        })
-        i++
-    }
-
-    const currentPhoto = reducedResults.find((img) => img.id === Number(context.params?.photoId))
-    if (currentPhoto) {
-        currentPhoto.blurDataUrl = await getBase64ImageUrl(currentPhoto)
-    }
-
-    return {
-        props: {
-            currentPhoto,
-        },
-    }
+    return createGetStaticProps(folderName, context)
 }
 
 export async function getStaticPaths() {
-    return await createGetStaticPaths(folderName)
+    return createGetStaticPaths(folderName)
 }
--- src/photos/createGetStaticProps.ts
+++ src/photos/createGetStaticProps.ts
@@ -1,34 +1,37 @@
 import { ImageProps } from '@src/photos/utils/types'
-import { GetStaticProps } from 'next'
+import { GetStaticPropsContext, GetStaticPropsResult } from 'next'
 import getResults from './utils/cachedImages'
 import getBase64ImageUrl from './utils/generateBlurPlaceholder'
 
-export const createGetStaticProps =
-    (folderName: string): GetStaticProps =>
-    async (context) => {
-        const results = await getResults(folderName)
-        const reducedResults: ImageProps[] = []
-        let i = 0
+export async function createGetStaticProps(
+    folderName: string,
+    context: GetStaticPropsContext
+): Promise<GetStaticPropsResult<{ currentPhoto: ImageProps }>> {
+    const results = await getResults(folderName)
+    const reducedResults: ImageProps[] = []
+    let i = 0
 
-        for (const result of results.resources) {
-            reducedResults.push({
-                id: i,
-                height: String(result.height),
-                width: String(result.width),
-                public_id: result.public_id,
-                format: result.format,
-            })
-            i++
-        }
-
-        const currentPhoto = reducedResults.find((img) => img.id === Number(context.params?.photoId))
-        if (currentPhoto) {
-            currentPhoto.blurDataUrl = await getBase64ImageUrl(currentPhoto)
-        }
-
-        return {
-            props: {
-                currentPhoto,
-            },
-        }
+    for (const result of results.resources) {
+        reducedResults.push({
+            id: i,
+            height: String(result.height),
+            width: String(result.width),
+            public_id: result.public_id,
+            format: result.format,
+        })
+        i++
     }
+
+    const currentPhoto = reducedResults.find((img) => img.id === Number(context.params?.photoId))
+    if (currentPhoto) {
+        currentPhoto.blurDataUrl = await getBase64ImageUrl(currentPhoto)
+    } else {
+        throw new Error('could find find photo in createGetStaticProps')
+    }
+
+    return {
+        props: {
+            currentPhoto,
+        },
+    }
+}
