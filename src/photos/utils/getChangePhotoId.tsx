916528ed19f9ef12894afb0d2c98e2c79ef281d2
Craig Patik <craig@patik.com>
2023-10-08 18:28:01 +0200

    fixed how the blurred background didn't always change when navigating

--- src/photos/utils/getChangePhotoId.tsx
+++ src/photos/utils/getChangePhotoId.tsx
@@ -11,7 +11,7 @@ export function getChangePhotoId({
 }: {
     setLastViewedPhoto: (newVal: number) => void
     setDirection: (newVal: number) => void
-    setCurrIndex?: (newVal: number) => void
+    setCurrIndex: (newVal: number) => void
     router: NextRouter
     index: number
     countryId: string
@@ -19,15 +19,12 @@ export function getChangePhotoId({
 }): (newVal: number) => void {
     return function changePhotoId(newVal: number): void {
         setLastViewedPhoto(newVal)
+        setCurrIndex(newVal)
 
         if (newVal > index) {
             setDirection(1)
         } else {
             setDirection(-1)
-        }
-
-        if (setCurrIndex) {
-            setCurrIndex(newVal)
         }
 
         const pathname = `/travel/${countryId}/photos/${cityId}/${newVal}`
