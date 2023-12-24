2cb371a4e702d43d392f847d09d9e50ee43d5ec6
Craig Patik <craig@patik.com>
2023-12-24 20:41:08 +0100

    fix test config

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,11 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-
-const { pathsToModuleNameMapper } = require('ts-jest')
-const { compilerOptions } = require('./tsconfig')
-
-
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
     rootDir: '.',
-    // moduleNameMapper: {
-    //     '^@pages/(.*)$': '<rootDir>/pages/$1',
-    //     '^@public/(.*)$': '<rootDir>/public/$1',
-    //     '^@src/(.*)$': '<rootDir>/src/$1',
-    // },
-    // modulePaths: [compilerOptions.baseUrl],
-    moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
+    moduleNameMapper: {
+        '^@pages/(.*)$': '<rootDir>/pages/$1',
+        '^@public/(.*)$': '<rootDir>/public/$1',
+        '^@src/(.*)$': '<rootDir>/src/$1',
+    },
 }
