1646fd52b5343b8951eb050188a174b1691b70b8
Craig Patik <craig@patik.com>
2023-12-24 20:35:26 +0100

    auto-import modules paths from tsconfig

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,19 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const { pathsToModuleNameMapper } = require('ts-jest')
+const { compilerOptions } = require('./tsconfig')
+
+
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
     rootDir: '.',
-    moduleNameMapper: {
-        '^@pages/(.*)$': '<rootDir>/pages/$1',
-        '^@public/(.*)$': '<rootDir>/public/$1',
-        '^@src/(.*)$': '<rootDir>/src/$1',
-    },
+    // moduleNameMapper: {
+    //     '^@pages/(.*)$': '<rootDir>/pages/$1',
+    //     '^@public/(.*)$': '<rootDir>/public/$1',
+    //     '^@src/(.*)$': '<rootDir>/src/$1',
+    // },
+    // modulePaths: [compilerOptions.baseUrl],
+    moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
 }
