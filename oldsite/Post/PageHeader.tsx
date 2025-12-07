17f795765aa92e9fe4e928885de322278bec21e9
Craig Patik <craig@patik.com>
2025-12-07 17:33:51 +0100

    delete oldsite folder

--- oldsite/Post/PageHeader.tsx
+++
@@ -1,9 +0,0 @@
-export default function PageHeader() {
-    return (
-        <p className="text-xl tracking-tight leading-tight mb-20 mt-8">
-            <a href={`.`} className="hover:underline">
-                &lt; Blog index
-            </a>
-        </p>
-    )
-}
--- oldsite/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-    content: [
-        './src/components/blog/**/*.tsx',
-        './app/**/*.{js,ts,jsx,tsx}',
-        './pages/**/*.{js,ts,jsx,tsx}',
-        './src/photos/**/*.{js,ts,jsx,tsx}',
-    ],
-}
