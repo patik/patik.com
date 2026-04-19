19578991afb7716fa0279f0346b0f6d71eedf942
Craig Patik <craig@patik.com>
2026-04-19 17:20:41 +0200

    Ireland 2022 (#261)

---
+++ src/galleries/ireland/belfast.ts
@@ -0,0 +1,12 @@
+import type { CityGallery } from '../../photos/utils/types'
+
+const cityGallery: CityGallery = {
+    countryId: 'ireland',
+    countryName: 'Ireland',
+    cityId: 'belfast',
+    cloudinaryFolder: '2022\\ Ireland/Belfast',
+    title: 'Belfast',
+    keywords: ['Ireland', 'Northern Ireland', 'Belfast', 'Europe', 'travel'],
+}
+
+export default cityGallery
---
+++ src/galleries/ireland/dublin.ts
@@ -0,0 +1,12 @@
+import type { CityGallery } from '../../photos/utils/types'
+
+const cityGallery: CityGallery = {
+    countryId: 'ireland',
+    countryName: 'Ireland',
+    cityId: 'dublin',
+    cloudinaryFolder: '2022\\ Ireland/Dublin',
+    title: 'Dublin',
+    keywords: ['Ireland', 'Dublin', 'Europe', 'travel'],
+}
+
+export default cityGallery
