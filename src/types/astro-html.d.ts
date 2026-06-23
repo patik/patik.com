25971633fb7b3125cafa27d96b09593c2f610804
Craig Patik <craig@patik.com>
2026-06-23 16:59:59 +0200

    Native Safari haptics for switches

---
+++ src/types/astro-html.d.ts
@@ -0,0 +1,6 @@
+declare namespace astroHTML.JSX {
+    interface InputHTMLAttributes {
+        /** A nonstandard attribute for triggering native haptics in Safari. https://christianoliff.com/blog/using-safaris-native-switch-with-bootstrap/ */
+        switch?: boolean | string | undefined | null
+    }
+}
