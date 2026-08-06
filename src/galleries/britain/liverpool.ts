0a53cd07e538f97cb27ea04a39b37515e535a8e8
Craig Patik <craig@patik.com>
2026-08-06 21:13:16 +0200

    Add gallery for Britain 2012

---
+++ src/galleries/britain/liverpool.ts
@@ -0,0 +1,12 @@
+import type { CityGallery } from '@src/photos/utils/types'
+
+const cityGallery: CityGallery = {
+    countryId: 'britain',
+    countryName: 'Britain',
+    cityId: 'liverpool',
+    cloudinaryFolder: '2012\\ Britain/Liverpool',
+    title: 'Liverpool',
+    keywords: ['United Kingdom', 'England', 'Liverpool', 'Beatles', 'Liverpool FC', 'Europe', 'travel'],
+}
+
+export default cityGallery
